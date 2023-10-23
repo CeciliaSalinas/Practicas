339436d5d1a5c36ffbc365e20a97c15534e73298
cecilialujansalinas@outlook.com <cecilialujansalinas@outlook.com>
2023-10-23 19:55:17 -0300

    método privado y publico

--- operadores/ClsAireAcondicionado.vb
+++ operadores/ClsAireAcondicionado.vb
@@ -1,12 +1,20 @@
 ﻿Public Class ClsAireAcondicionado
 
     'ATRIBUTOS/valores DE LA CLASE (los atributos son las variables)
-    Public marca As String
-    Public modelo As String
-    Public color As String
-    Public voltaje As Integer
-    Public valor As Integer
-    Public temperatura As Integer
+    'Public marca As String
+    'Public modelo As String
+    'Public color As String
+    'Public voltaje As Integer
+    'Public valor As Integer
+    'Public temperatura As Integer
+
+    'ENCAPSULAMIENTO CON MODIFICADOR DE ACCESO PRIVATE
+    Private marca As String
+    Private modelo As String
+    Private color As String
+    Private voltaje As Integer
+    Private valor As Integer
+    Private temperatura As Integer
 
     'METODOS DE LA CLASE
     Public Sub subirTemperatura()
@@ -16,5 +24,54 @@
     Public Sub bajarTemperatura()
         temperatura -= 1
     End Sub
+
+    Public Sub setMarca(mar As String)
+        marca = mar
+    End Sub
+
+    Public Function getMarca() As String
+        Return marca
+    End Function
+
+    Public Sub setModelo(mo As String)
+        modelo = mo
+    End Sub
+
+    Public Function getModelo() As String
+        Return modelo
+    End Function
+
+    Public Sub setColor(col As String)
+        color = col
+    End Sub
+
+    Public Function getColor() As String
+        Return color
+    End Function
+
+    Public Sub setVoltaje(vol As Integer)
+        voltaje = vol
+    End Sub
+
+    Public Function getVoltaje() As Integer
+        Return voltaje
+    End Function
+
+
+    Public Sub setValor(val As Integer)
+        valor = val
+    End Sub
+
+    Public Function getValor() As Integer
+        Return valor
+    End Function
+
+    Public Sub setTemperatura(temp As Integer)
+        temperatura = temp
+    End Sub
+
+    Public Function getTemperatura() As Integer
+        Return temperatura
+    End Function
 
 End Class
--- operadores/CreacionObjeto.vb
+++ operadores/CreacionObjeto.vb
@@ -4,19 +4,45 @@
         'creamos objeto que va a perteneracer a la clase a.acondiconado
         Dim sams As ClsAireAcondicionado = New ClsAireAcondicionado()
 
-        'asignamos valores a los atributos con el . (punto)
-        sams.marca = "sams"
-        sams.modelo = "sams25"
-        sams.color = " black"
-        sams.voltaje = 220
-        sams.valor = 400.0
-        sams.temperatura = 0
+        'asignamos valores a los atributos con el . (punto)************
+        'sams.marca = "sams"
+        'sams.modelo = "sams25"
+        'sams.color = " black"
+        'sams.voltaje = 220
+        'sams.valor = 400.0
+        'sams.temperatura = 30
 
-        Console.WriteLine(sams.marca)
-        Console.WriteLine(sams.modelo)
-        Console.WriteLine(sams.color)
-        Console.WriteLine(sams.voltaje)
-        Console.WriteLine(sams.valor)
-        Console.WriteLine(sams.temperatura)
+
+        'ACCESO AL VALOR POR PRIVATE
+        sams.setMarca("tyt")
+        sams.setModelo("ghj76")
+        sams.setColor("pink")
+        sams.setVoltaje(220)
+        sams.setValor(400000)
+        sams.setTemperatura(17)
+
+        'MUESTRO POR CONSOLA EL VALOR DE LOS ATRIBUTOS ***************
+        'Console.WriteLine(sams.marca)
+        'Console.WriteLine(sams.modelo)
+        'Console.WriteLine(sams.color)
+        'Console.WriteLine(sams.voltaje)
+        'Console.WriteLine(sams.valor)
+        'Console.WriteLine(sams.temperatura)
+
+
+        'MUESTRO POR CONSOLA EL VALOR DE LOS ATRIBUTOS DE UN METODO PRIVADO
+        Console.WriteLine(sams.getMarca())
+        Console.WriteLine(sams.getModelo())
+        Console.WriteLine(sams.getColor())
+        Console.WriteLine(sams.getVoltaje())
+        Console.WriteLine(sams.getValor())
+        Console.WriteLine(sams.getTemperatura())
+
+        'USAR UN MÉTODO DEL OBJETO
+        'sams.subirTemperatura()
+
+        'MUESTRA DATOS POR CONSOLA
+        'Console.WriteLine(sams.temperatura)
+
     End Sub
 End Module
